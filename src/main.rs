44c550462638dc4ded23749ec6a7faac5bddc264
zeno <zacheryklinglesmith@gmail.com>
2023-09-14 20:54:15 -0400

    Extract Screen struct for drawing to the terminal

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,17 @@
-use crossterm::{
-    cursor::{Hide, Show, MoveTo},
+mod screen;
+use {
+    std::time::Duration,
+    screen::Screen,
+    crossterm::{
     event::{poll, read, Event, KeyCode},
-    execute, queue, style,
-    style::Stylize,
-    terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use std::{
-    io,
-    io::{stdout, Write},
-    time::Duration,
+    }
 };
 
-fn main() -> io::Result<()> {
-    let mut stdout = stdout();
-    let (width, height) = size()?;
-    let [x, y] = [width / 2, height / 2];
-
-    execute!(stdout, EnterAlternateScreen, Hide)?;
-    enable_raw_mode()?;
+fn main() -> std::io::Result<()> {
+    let mut screen = Screen::new()?;
 
     'main: loop {
-        execute!(stdout, Clear(ClearType::All))?;
-        queue!(
-            stdout,
-            MoveTo(x, y),
-            style::PrintStyledContent("@".white()),
-        )?;
-        stdout.flush()?;
+        screen.draw()?;
 
         if poll(Duration::from_millis(1))? {
             let event = read()?;
@@ -37,7 +22,5 @@ fn main() -> io::Result<()> {
         }
     }
 
-    execute!(stdout, LeaveAlternateScreen, Show)?;
-    disable_raw_mode()?;
     Ok(())
 }
---
+++ src/screen.rs
@@ -0,0 +1,54 @@
+use {
+    crossterm::{
+        cursor::{Hide, MoveTo, Show},
+        execute, queue,
+        style::{PrintStyledContent, Stylize},
+        terminal::{
+            disable_raw_mode, enable_raw_mode, size, Clear, ClearType, EnterAlternateScreen,
+            LeaveAlternateScreen,
+        },
+    },
+    std::{
+        io::{stdout, Write, Stdout},
+    },
+};
+
+pub struct Screen {
+    width: u16,
+    height: u16,
+    stdout: Stdout,
+}
+
+impl Screen {
+    pub fn new() -> std::io::Result<Self> {
+        let mut stdout = stdout();
+        let (width, height) = size()?;
+
+        execute!(stdout, EnterAlternateScreen, Hide)?;
+        enable_raw_mode()?;
+
+        Ok(Self {
+            width,
+            height,
+            stdout,
+        })
+    }
+
+    pub fn draw(&mut self) -> std::io::Result<()> {
+        let [x, y] = [self.width / 2, self.height / 2];
+
+        execute!(self.stdout, Clear(ClearType::All))?;
+
+        queue!(self.stdout, MoveTo(x, y), PrintStyledContent("@".white()),)?;
+        
+        self.stdout.flush()
+    }
+}
+
+impl Drop for Screen {
+    fn drop(&mut self) {
+        disable_raw_mode()
+            .and_then(|()| execute!(self.stdout, LeaveAlternateScreen, Show))
+            .expect("error dropping screen");
+    }
+}
